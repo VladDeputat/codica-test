5eecd8dffbdbfa8219c0155bc8968a0281db9158
Vlad Deputat <deputatwladek@gmail.com>
2022-12-16 16:50:53 +0200

    pre deploy changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CityPage from "./pages/CityPage";
-import NotFoundPage from "./pages/NotFoundPage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CityPage from './pages/CityPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index path="/" element={<HomePage />} />
-        <Route path=":city" element={<CityPage />} />
+        <Route path="/:city" element={<CityPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
